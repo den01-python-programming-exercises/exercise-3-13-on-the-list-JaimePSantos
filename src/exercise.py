d779b8a5c9d2266fff5aafc6180f5e05c2316faf
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-22 11:15:25 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #write your code below this line
-
+  strList = []
+  while(True):
+    inputStr = input()
+    if(inputStr==""):
+      break
+    strList.append(inputStr)
+  
+  searchStr = input("Search for?")
+  for i in range(len(strList)):
+    if(searchStr == strList[i]):
+      print("%s was found!"%searchStr)
+      break
+    elif(i==len(strList)-1):
+      print(("%s was not found!"%searchStr))
 if __name__ == '__main__':
     main()
